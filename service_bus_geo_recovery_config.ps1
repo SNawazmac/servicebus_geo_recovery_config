e7e6487364d3091d41300c36ff66afd78b3e6c55
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-26 14:36:21 +0530

    Update service_bus_geo_recovery_config.ps1

--- service_bus_geo_recovery_config.ps1
+++ service_bus_geo_recovery_config.ps1
@@ -1,11 +1,13 @@
 param( 
 [Parameter(Mandatory=$true)][string]$primary_servicebus_resource_group_name,                         #Enter the resourcegroup name of servicebus on which geo-recovery needs to be configured
-[Parameter(Mandatory=$true)][string]$primary_servicebus_aliasname,                                   #Enter the alias name     
+[Parameter(Mandatory=$true)][string]$servicebus_aliasname,                                           #Enter the alias name     
 [Parameter(Mandatory=$true)][string]$primary_servicebus_namespace_name,                              #Enter the name of the namespace on which geo-recovery needs to be configured
-[Parameter(Mandatory=$true)][string]$secondary_servicebus_namespaceId                                #Enter the resource ID of the namespace with which the pairing is to be initiated       
+[Parameter(Mandatory=$true)][string]$secondary_servicebus_namespaceId,                               #Enter the resource ID of the namespace with which the pairing is to be initiated
+[Parameter(Mandatory=$true)][string]$primary_subscription_id                                                
 )
 
+Set-AzContext -Subscription $primary_subscription_id
 
-New-AzServiceBusGeoDRConfiguration -Name $primary_servicebus_aliasname -NamespaceName $primary_servicebus_namespace_name -ResourceGroupName $primary_servicebus_resource_group_name -PartnerNamespace $secondary_servicebus_namespaceId
+New-AzServiceBusGeoDRConfiguration -Name $servicebus_aliasname -NamespaceName $primary_servicebus_namespace_name -ResourceGroupName $primary_servicebus_resource_group_name -PartnerNamespace $secondary_servicebus_namespaceId
 
    
